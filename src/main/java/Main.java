4574f99a3f5c3f95903cc9cf2674c0ab05b583e6
zrafal <161606153+zrafal@users.noreply.github.com>
2024-06-17 21:03:57 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+    String namme = "Hello World";
+     for (int i = 0; i<namme.length(); i++) {
+        System.out.println(namme.charAt(i));
+      }
+    }
+
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
